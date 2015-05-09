7782483b9f462efb4d034160586aefbc64a7eafe
florian <florian.jeanmart@factorx.eu>
2015-05-09 13:24:39 +0200

    e

--- public/javascripts/service/FacebookService.js
+++ public/javascripts/service/FacebookService.js
@@ -1,8 +1,15 @@
-myApp.service("facebookService", function (Facebook) {
+myApp.service("facebookService", function () {
 
-    this.login = function(successCallback,failCallback) {
+    FB.init({
+        appId: '1432915530336007',
+        cookie: true,
+        xfbml: true,
+        version: 'v2.3'
+    });
+
+    this.login = function (successCallback, failCallback) {
         // From now on you can use the Facebook service just as Facebook api says
-        Facebook.login(function(response) {
+        FB.login(function (response) {
             console.log('FB.login');
             console.log(response);
 
@@ -10,30 +17,30 @@ myApp.service("facebookService", function (Facebook) {
             var user_id = response.authResponse.userID;
 
             //TODO manage error
-            this.loginToServer(access_token,user_id,successCallback,failCallback);
+            this.loginToServer(access_token, user_id, successCallback, failCallback);
         });
     };
 
-    this.getLoginStatus = function() {
+    this.getLoginStatus = function () {
         console.log('FB.getLoginStatus start');
-        Facebook.getLoginStatus(function(response) {
+        FB.getLoginStatus(function (response) {
             console.log('FB.getLoginStatus');
             console.log(response);
-            if(response.status === 'connected') {
+            if (response.status === 'connected') {
                 var access_token = response.authResponse.accessToken;
                 var user_id = response.authResponse.userID;
-                this.loginToServer(access_token,user_id,function(){
-                    console.log('success');
-                },
-                function(){
-                    console.log('faild');
-                });
+                this.loginToServer(access_token, user_id, function () {
+                        console.log('success');
+                    },
+                    function () {
+                        console.log('faild');
+                    });
             } else {
             }
         });
     };
 
-    this.loginToServer = function (access_token, user_id,successCallback,failCallback) {
+    this.loginToServer = function (access_token, user_id, successCallback, failCallback) {
 
         //send request
         var dto = {
@@ -48,10 +55,10 @@ myApp.service("facebookService", function (Facebook) {
             'data': dto
         }).success(function (data, status) {
             //store connected user
-            modelService.store(modelService.MY_SELF,data.myself);
+            modelService.store(modelService.MY_SELF, data.myself);
             //test lang
             var lang = $locale.id.split("-")[0];
-            if(data.myself.lang.code != lang){
+            if (data.myself.lang.code != lang) {
 
             }
 
